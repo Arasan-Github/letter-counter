baa91145f30997238fa2c674dc7624c1e7fcc37a
Jayaseela Arasan <jayaseelaarasan.f@gmail.com>
2026-01-08 05:43:48 +0530

    removed unused import statements and played around

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import WordLetterCounter from "./WordLetterCounter";
 
 function App() {
-
   return (
     <>
       <div className="App">
-        <h1 id="top">Pirated project</h1>
+        <h1
+          id="top"
+          style={{
+            fontFamily: "Arial, sans-serif",
+            fontSize: "100px"
+          }}
+        >
+          Pirated project
+        </h1>
         <h1>Word & letter counter</h1>
         <WordLetterCounter />
       </div>
